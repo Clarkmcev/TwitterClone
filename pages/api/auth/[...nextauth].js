8341305c886db087341da36dd556db50d3ec4281
Clark68500 <mcevoyclark@gmail.com>
2022-10-26 16:17:21 +0200

    vercel env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,7 +4,27 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
+    process.env.VERCEL_ENV === "preview"
+    ? CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          username: {
+            label: "Username",
+            type: "text",
+            placeholder: "jsmith",
+          },
+          password: { label: "Password", type: "password" },
+        },
+        async authorize() {
+          return {
+            id: 1,
+            name: "J Smith",
+            email: "jsmith@example.com",
+            image: "https://i.pravatar.cc/150?u=jsmith@example.com",
+          }
+        },
+      })
+    : GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
