1af32ebd3c5b97674baaa2274d102398eae9ebb2
Clark68500 <mcevoyclark@gmail.com>
2022-10-26 18:00:46 +0200

    config adjustments

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,27 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    process.env.VERCEL_ENV === "preview"
-    ? CredentialsProvider({
-        name: "Credentials",
-        credentials: {
-          username: {
-            label: "Username",
-            type: "text",
-            placeholder: "jsmith",
-          },
-          password: { label: "Password", type: "password" },
-        },
-        async authorize() {
-          return {
-            id: 1,
-            name: "J Smith",
-            email: "jsmith@example.com",
-            image: "https://i.pravatar.cc/150?u=jsmith@example.com",
-          }
-        },
-      })
-    : GoogleProvider({
+  GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
